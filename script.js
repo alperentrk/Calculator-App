9384dae18bdaf95bcc406d870c4192ce6fdaa6ef
Alperen <alperenturk5205@gmail.com>
2025-02-01 00:24:15 +0300

    İlk Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }else{
+        handleNumber(value);
+    }
+    screen.innerText = buffer;
+}
+
+function handleSymbol(symbol){
+    switch(symbol){
+        
+    }
+}
